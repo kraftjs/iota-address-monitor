86efb7548f0833ffc8e2c6c01b6224d162c5ea4e
Jason Kraft <developer.kraft@gmail.com>
2021-10-18 11:53:31 -0700

    Remove template styling and add functionality to store/retrieve data in localStorage

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,32 @@
 <script lang="ts">
-	export let name: string;
+    import {onMount} from 'svelte';
+
+    let addresses: Array<string> = [];
+    let address = '';
+
+    onMount(() => {
+        addresses = JSON.parse(window.localStorage.getItem('storedAddresses') || '[]');
+    })
+
+    function handleSubmit() {
+        addresses = [...addresses, address];
+        window.localStorage.setItem('storedAddresses', JSON.stringify(addresses));
+        address = '';
+    }
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
 
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
+
+
+<form on:submit|preventDefault={handleSubmit}>
+    <input type="text" placeholder="Enter address" bind:value={address}>
+    <button type="submit">Add</button>
+</form>
+<ul>
+    {#each addresses as address}
+        <li>{address}</li>
+    {/each}
+</ul>
