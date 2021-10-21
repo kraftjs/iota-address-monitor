06618846803643b58c4f29cab1d9dedeadcbd284
Jason Kraft <developer.kraft@gmail.com>
2021-10-20 18:12:42 -0700

    Add functionality to allow users to select between mainnet and devnet networks

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -1,21 +1,53 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
+    enum NetworkOptions {
+        Devnet = 'devnet',
+        Mainnet = 'mainnet',
+    }
+
+    let network: NetworkOptions = JSON.parse(window.localStorage.getItem('preferredNetwork') || '"devnet"');
     let addresses: Array<string> = [];
     let address = '';
 
     onMount(() => {
-        addresses = JSON.parse(window.localStorage.getItem('storedAddresses') || '[]');
+        addresses = JSON.parse(
+            window.localStorage.getItem(network === NetworkOptions.Devnet ? 'devnetAddresses' : 'mainnetAddresses') ||
+                '[]',
+        );
     });
+
+    $: {
+        window.localStorage.setItem('preferredNetwork', JSON.stringify(network));
+        addresses = JSON.parse(
+            window.localStorage.getItem(network === NetworkOptions.Devnet ? 'devnetAddresses' : 'mainnetAddresses') ||
+                '[]',
+        );
+    }
 
     function handleSubmit() {
         addresses = [...addresses, address];
-        window.localStorage.setItem('storedAddresses', JSON.stringify(addresses));
+        window.localStorage.setItem(
+            network === NetworkOptions.Devnet ? 'devnetAddresses' : 'mainnetAddresses',
+            JSON.stringify(addresses),
+        );
         address = '';
     }
 </script>
 
 <form on:submit|preventDefault={handleSubmit}>
+    <p>Please select which network to monitor:</p>
+    <div>
+        <label>
+            <input type="radio" bind:group={network} name="network" value={NetworkOptions.Devnet} />
+            devnet
+        </label>
+
+        <label>
+            <input type="radio" bind:group={network} name="network" value={NetworkOptions.Mainnet} />
+            mainnet
+        </label>
+    </div>
     <input type="text" placeholder="Enter address" bind:value={address} />
     <button type="submit">Add</button>
 </form>
