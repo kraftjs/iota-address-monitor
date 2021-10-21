83ceada01e9792c7200073d98d4d6ba71b226297
Jason Kraft <developer.kraft@gmail.com>
2021-10-21 14:05:48 -0700

    Add functionality to get addresses

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { onMount } from 'svelte';
 
-    import { Bech32Helper } from '@iota/iota.js';
+    import { Bech32Helper, SingleNodeClient } from '@iota/iota.js';
 
     enum NetworkType {
         Dev = 'devnet',
@@ -19,20 +19,35 @@
         Mainnet = 'https://chrysalis-nodes.iota.cafe/',
     }
 
+    interface Address {
+        address: string;
+        balance: number;
+    }
+
     let currentNetwork: NetworkType = JSON.parse(window.localStorage.getItem(LocalStorage.SavedNetwork) || '"devnet"');
     let currentStorageKey: LocalStorage.DevAddresses | LocalStorage.MainAddresses;
     let addresses: Array<string> = [];
+    let addressObjectList: Array<Address> = [];
     let address = '';
+    let client = {
+        devnet: new SingleNodeClient(NodeEndpoint.Devnet),
+        mainnet: new SingleNodeClient(NodeEndpoint.Mainnet),
+    };
+    let currentClient: SingleNodeClient;
 
-    onMount(() => {
+    onMount(async () => {
         addresses = JSON.parse(window.localStorage.getItem(currentStorageKey) || '[]');
+        addressObjectList = await retrieveBalances(addresses, currentClient);
     });
 
     $: {
         window.localStorage.setItem(LocalStorage.SavedNetwork, JSON.stringify(currentNetwork));
         currentStorageKey = currentNetwork === NetworkType.Dev ? LocalStorage.DevAddresses : LocalStorage.MainAddresses;
+        currentClient = currentNetwork === NetworkType.Dev ? client.devnet : client.mainnet;
         addresses = JSON.parse(window.localStorage.getItem(currentStorageKey) || '[]');
     }
+
+    $: (async () => (addressObjectList = await retrieveBalances(addresses, currentClient)))();
 
     function isValidBech32(address: string): boolean {
         try {
@@ -44,6 +59,18 @@
         } catch (e) {
             return false;
         }
+    }
+
+    async function retrieveBalance(address: string, client: SingleNodeClient): Promise<Address> {
+        const addressInfo = await client.address(address);
+        return {
+            address,
+            balance: addressInfo.balance,
+        };
+    }
+
+    async function retrieveBalances(addresses: string[], client: SingleNodeClient): Promise<Address[]> {
+        return Promise.all(addresses.map((address) => retrieveBalance(address, client)));
     }
 
     function handleSubmit() {
@@ -78,8 +105,8 @@
     <button type="submit">Add</button>
 </form>
 <ul>
-    {#each addresses as address (address)}
-        <li><button type="button" on:click={() => handleDelete(address)}>Delete</button>{address}</li>
+    {#each addressObjectList as { address, balance } (address)}
+        <li><button type="button" on:click={() => handleDelete(address)}>Delete</button>{address}{balance}</li>
     {/each}
 </ul>
 
